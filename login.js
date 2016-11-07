1c7449eba2ff0aefc798ee1dafc197376ad372ab
xzc5858 <x_zc@qq.com>
2016-11-07 22:48:15 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 import React, { Component } from 'react';
 import {
     AppRegistry,
@@ -8,22 +8,24 @@ import {
     Text,
     TextInput,
     View
-} from 'react-native';
+    } from 'react-native';
 const len = 50;
 export default class login extends Component {
     render() {
         return (
             <View style={Styles.container}>
-    <View><Image source={require('../images/logo.png')} style={Styles.logo}></Image></View>
-       <TextInput underlineColorAndroid = "transparent" style={Styles.input1} placeholder="手机或邮箱" autoCorrect={true}></TextInput>
-       <TextInput underlineColorAndroid = "transparent" style={Styles.input2} placeholder="密码" password={true}></TextInput>
-        <View><Text>登陆</Text></View>
-        <View><Text style={Styles.txt}>忘记密码?</Text></View>
-            <View style={{height:50}}></View>
-        <View><Text style={Styles.txt}>---社交账号直接登记---</Text></View>
-        <View></View>
-        <View style={Styles.newUser}><Text>注册新用户</Text><Text>｜</Text><Text>国外手机注册</Text></View>
-        </View>);
+                <View><Image source={require('../images/logo.png')} style={Styles.logo}></Image></View>
+
+                     <TextInput underlineColorAndroid = "transparent" style={Styles.input1} placeholder="手机或邮箱" autoCorrect={true}></TextInput>
+                     <TextInput underlineColorAndroid = "transparent" style={Styles.input2} placeholder="密码" password={true}></TextInput>
+
+                <View><Text>登陆</Text></View>
+                <View><Text style={Styles.txt}>忘记密码?</Text></View>
+                <View style={{height:50}}></View>
+                <View><Text style={Styles.txt}>---社交账号直接登记---</Text></View>
+                <View></View>
+                <View style={Styles.newUser}><Text>注册新用户</Text><Text>｜</Text><Text>国外手机注册</Text></View>
+            </View>);
     }
 }
 var Styles = StyleSheet.create({
@@ -31,7 +33,7 @@ var Styles = StyleSheet.create({
     container:{
         alignItems: 'center',
         flex:1,
-        flexDirection: 'row',
+
     },
     logo:{
         width:50,
